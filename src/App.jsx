9ca641955de64ef15b2bde420fb12e86900801d0
ChristopherItuma <itumachristopherchukwudi@gmail.com>
2025-03-14 13:30:56 +0100

    index.css theme configuration

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='text-red-500'>Hello Friends</h1>
+      <h1 className='text-accent bg-primary'>Hello Friends</h1>
     </div>
   )
 }
